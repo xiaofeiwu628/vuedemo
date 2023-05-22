556d78f0e096996121beccc0ea9cbd1f4c97b577
Jinjiyang <1919739839@qq.com>
2023-05-22 09:04:30 +0800

    添加注释

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 
 <template>
-
+<!--      template下面不能有两个根组件并列，如果组合报错请适当利用div把他们组在一块      -->
   <div>
-
+<!--   格式控制 -->
     <el-row>
       <el-col
             :span="24"
@@ -13,13 +13,14 @@
               <h1>达成度查询</h1>
               <el-row :gutter="5">
                 <el-col :span="10">
-
+                  <!--  第一个查询组件  -->
                   <el-form :inline="true" :model="formInline"  class="demo-form-inline" >
                     <el-form-item label="专业">
                       <el-input v-model="formInline.majorid" placeholder="专业"></el-input>
                     </el-form-item>
                     <el-form-item label="年级">
                       <el-select v-model="formInline.grade" placeholder="年级">
+<!--                        自定义年级       -->
                         <el-option label="2019" value="2019"></el-option>
                         <el-option label="2020" value="2020"></el-option>
                       </el-select>
@@ -31,7 +32,7 @@
 
                 </el-col>
                 <el-col :span="14">
-
+                  <!-- 第二个查询组件 -->
                   <el-form :inline="true" :model="formInline1" :label-position="right" :label-width="auto" class="demo-form-inline1">
                     <el-form-item label="专业">
                       <el-input v-model="formInline1.majorid" placeholder="专业"></el-input>
@@ -40,6 +41,7 @@
                       <el-select v-model="formInline1.requirements" placeholder="要求">
                         <el-option label="1" value="1"></el-option><el-option label="2" value="2"></el-option><el-option label="3" value="3"></el-option><el-option label="4" value="4"></el-option>
                         <el-option label="5" value="5"></el-option><el-option label="6" value="6"></el-option><el-option label="7" value="7"></el-option><el-option label="8" value="8"></el-option>
+                        <el-option label="9" value="9"></el-option><el-option label="10" value="10"></el-option><el-option label="11" value="11"></el-option><el-option label="12" value="12"></el-option>
                       </el-select>
                     </el-form-item>
                     <el-form-item>
@@ -49,17 +51,18 @@
 
                 </el-col>
               </el-row>
+<!--              图标作图的div区域      -->
               <div id="echarts_box" style="width: 600px;height:400px;float: left;display: inline"></div>
               <div id="echarts_box2" style="width: 600px;height:400px;float: right;display: inline"></div>
+
             </div>
           </el-card>
 
-<!--          <el-card>-->
 
-<!--          </el-card>-->
 
         </el-col>
       </el-row>
+
   </div>
 
 
@@ -68,6 +71,7 @@
 </template>
 
 <script>
+  // 引入echarts
 import * as echarts from 'echarts';
 import request from "@/utils/request";
 import {left, right} from "core-js/internals/array-reduce";
@@ -78,20 +82,23 @@ export default {
   data() {
 
     return {
+      // 专业年级查询，第一个查询组件的数据
       formInline: {
         grade: '',
         majorid: ''
       },
-
+      // 第一个图表的数据
       tableData1:{
         xlable:[],
         ydata:[]
 
       },
+      // 毕业要求查询，第二个查询组件的数据
       formInline1:{
         majorid: '',
         requirements: ''
     },
+      // 第二个图表的数据
       tableData2:{
         xlable:[],
         ydata:[]
@@ -106,111 +113,43 @@ export default {
   },
   //挂载，渲染完成触发
   async mounted() {
-  //   // 基于准备好的dom，初始化echarts实例
-  //   this.myChart = echarts.init(document.getElementById('echarts_box'))
-  //   this.myChart2 = echarts.init(document.getElementById('echarts_box2'))
-  //   this.myChart3 = echarts.init(document.getElementById('echarts_box3'))
-  //
-  //   //发起请求，获取数据
-  //
-  //   var option = {
-  //     title: {
-  //       text: 'ECharts 入门示例'
-  //     },
-  //     tooltip: {},
-  //     xAxis: {
-  //       data: ["衬衫", "羊毛衫", "雪纺衫", "裤子"]
-  //     },
-  //     yAxis: {},
-  //     series: [{
-  //       name: '销量',
-  //       type: 'bar',
-  //       data: [5, 20, 36, 10]
-  //     }]
-  //   };
-  //   var option2 = {
-  //     title: {
-  //       text: 'ECharts 入门示例'
-  //     },
-  //     tooltip: {},
-  //     xAxis: {
-  //       data: ["衬衫2", "羊毛衫2", "雪纺衫2", "裤子2"]
-  //     },
-  //     yAxis: {},
-  //     series: [{
-  //       name: '销量2',
-  //       type: 'bar',
-  //       data: [5, 20, 36, 10]
-  //     }]
-  //   };
-  //   var option3 = {
-  //     // 标题
-  //     title: {
-  //       text: '用户来源'
-  //     },
-  //     // 提示框
-  //     tooltip: {
-  //       trigger: "axis",
-  //       axisPointer: {
-  //         type: "cross",
-  //         label: {
-  //           backgroundColor: "#76baf1"
-  //         }
-  //       }
-  //     },
-  //     // 图例
-  //     legend: {
-  //       data: ["华东","华南","华北","西部","其他"]
-  //     },
-  //     // x轴
-  //     xAxis: {
-  //       data: ["2017-12-27","2017-12-28","2017-12-29","2017-12-30","2017-12-31", "2018-1-1"]
-  //     },
-  //     yAxis: {},
-  //     // 数据,type:图表类型,stack:数据堆叠,areaStyle:区域填充样式,normal:默认,smooth:平滑曲线
-  //     series: [
-  //       { name: "华东", type: "line", stack: "总量", areaStyle: {normal: {}}, data: [999,3111,4100,3565,3528,6000] },
-  //       { name: "华南", type: "line", stack: "总量", areaStyle: {normal: {}}, data: [5090,2500,3400,6000,6400,7800] },
-  // ]
-  //
-  // };
-  //   // 绘制图表
-  //   this.myChart.setOption(option)
-  //   this.myChart2.setOption(option2)
-  //   this.myChart3.setOption(option3)
-
 
   },
   methods: {
     right,
     left,
+    //提交查询
     onSubmit() {
       console.log('专业年级查询');
       console.log(this.formInline)
+      //获取数据
       this.findDataList(this.formInline.grade,this.formInline.majorid)
     },
+    //提交查询
     onSubmit1(){
       console.log('毕业要求查询')
       console.log(this.formInline1)
       this.findDataList1(this.formInline1.majorid,this.formInline1.requirements)
 
     },
+    //获取数据
     findDataList(){
+      //获取数据,链接直接到后台，自己看着改链接
+      this.request.get("/data/findDataList",{
+        params:{
+          grade1: this.formInline.grade,
+          majorid1: this.formInline.majorid
+        }
+      }).then(res=>{
+        //打印数据
+        console.log(res)
+        //渲染图表
+        this.RenderEcharts(res.data)
+      }).catch(err=>{
+        console.log(err)
+      })
 
-      // this.request.get("/data/findDataList",{
-      //   params:{
-      //     grade1: this.formInline.grade,
-      //     majorid1: this.formInline.majorid
-      //   }
-      // }).then(res=>{
-      //   console.log(res)
-      //
-      //   this.RenderEcharts(res.data)
-      // }).catch(err=>{
-      //   console.log(err)
-      // })
-
-      //测试数据
+      //测试用数据，如果是用后台获取数据，就注释掉151-162行
       var data = [
         {name:  "指标1",value:  0.76},
         {name:  "指标2",value:  0.79},
@@ -223,6 +162,7 @@ export default {
       ]
       this.RenderEcharts(data)
     },
+    //仍然是获取数据，结构同上
     findDataList1(){
       // this.request.get("/data/findDataList1",{
       //   params:{
@@ -243,7 +183,7 @@ export default {
       ]
       this.RenderEcharts1(data1)
     },
-    //作图，obj是作图需要的数据对象
+    //作图，obj是作图需要的数据对象，也就是前面获取的数据，例如测试数据data或者res.data
     RenderEcharts(obj) {
       this.tableData1.xlable = []
       this.tableData1.ydata = []
@@ -251,27 +191,36 @@ export default {
         this.tableData1.xlable.push(obj[i].name)
         this.tableData1.ydata.push(obj[i].value)
       }
+      // 基于准备好的dom，初始化echarts实例
       this.myChart = echarts.init(document.getElementById('echarts_box'))
-
+      // 绘制图表框架
       let option = {
+        // 标题
         title: {
           text: '毕业要求达成度'
         },
         tooltip: {},
+        // 图例x轴
         xAxis: {
           data: []
         },
+        // 图例y轴
         yAxis: {},
+        // 数据
         series: [{
           name: '达成度',
           type: 'effectScatter',
           data: []
         }]
       };
-
+      // 使用刚指定的配置项和数据显示图表。如果在option里直接this.的话，会报错，例如
+      // xAxis: {
+      //data: this.tableData1.xlable
+      //},
+      // 所以用option传递一下数据
       option.xAxis.data = this.tableData1.xlable
       option.series[0].data = this.tableData1.ydata
-
+      // 渲染图表
       this.myChart.setOption(option)
     },
     RenderEcharts1(obj1){
